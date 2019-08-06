d7ee284d7d51b0ae94f2a25bf33839c61139c6cc
casualshay <akshaysprabhakar@gmail.com>
2019-08-05 23:35:58 -0700

    button added

---
+++ src/components/todoInput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './todoInput.css';
+
+export default class TodoInput extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {value: "test"};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.addTodo = this.addTodo.bind(this);
+	}
+
+	handleChange(e) {
+		console.log("change here");
+	}
+
+	addTodo(todo) {
+		console.log("TODO: ", todo);
+	}
+
+	render() {
+		return (
+			<div>
+				<input type="text" value="" onChange={this.handleChange} />
+				<button className="btn btn-primary" onclick={() => this.addTodo(this.state.value)}>Submit</button>
+			</div>
+		);
+	}
+}
+
